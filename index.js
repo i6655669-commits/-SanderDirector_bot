dee0329c9d9a07fc24a8bd0af43ceb13854517ad
i6655669-commits <i6655669@gmail.com>
2026-07-13 21:58:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 import type { InlineKeyboardMarkup } from "node-telegram-bot-api";
+
 import fs from "node:fs";
 import path from "node:path";
 import { logger } from "../lib/logger";
