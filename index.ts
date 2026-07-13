75a884ce34aae435723fac56a935e8a962ee7031
i6655669-commits <i6655669@gmail.com>
2026-07-14 00:24:37 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ import {
   bankWithdraw,
   getTopPlayers,
   resetBalance,
-} from "./players";
+} from "../players";
 
 // ─────────────────────────────────────────────────────────────────────────────
 // КОНФИГУРАЦИЯ
